3d61f6d354f14e22754d25b380a13f9b9e87e86b
Tomasz Adamski <tomasz.adamski@gmail.com>
2013-04-08 21:34:19 +0200

    reverted new icon, ufff

--- src/main/java/com/tadamski/arij/worklog/notification/NewWorklogNotificationBuilder.java
+++ src/main/java/com/tadamski/arij/worklog/notification/NewWorklogNotificationBuilder.java
@@ -4,13 +4,13 @@
  */
 package com.tadamski.arij.worklog.notification;
 
-import android.R;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import com.tadamski.arij.R;
 import com.tadamski.arij.issue.Issue;
 import com.tadamski.arij.login.LoginInfo;
 import com.tadamski.arij.worklog.activity.NewWorklogActivity_;
@@ -38,8 +38,8 @@ public class NewWorklogNotificationBuilder {
 
         Notification notification =
                 new Notification.Builder(ctx).
-                setLargeIcon(BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_menu_agenda)).
-                setSmallIcon(R.drawable.ic_menu_agenda).
+                setLargeIcon(BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_stat_new_worklog)).
+                setSmallIcon(R.drawable.ic_stat_new_worklog).
                 setOngoing(true).
                 setContentTitle(issue.getSummary().getKey() + ": " + issue.getSummary().getSummary()).
                 setAutoCancel(false).
