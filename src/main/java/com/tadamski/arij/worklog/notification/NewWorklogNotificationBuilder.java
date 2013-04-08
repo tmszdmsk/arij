b3ef3f2c77ae672e3a596afc3049a3ccf29e7801
Tomasz Adamski <tomasz.adamski@gmail.com>
2013-04-08 19:43:18 +0200

    added notification icon for new worklog

--- src/main/java/com/tadamski/arij/worklog/notification/NewWorklogNotificationBuilder.java
+++ src/main/java/com/tadamski/arij/worklog/notification/NewWorklogNotificationBuilder.java
@@ -4,13 +4,13 @@
  */
 package com.tadamski.arij.worklog.notification;
 
-import android.R;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import com.tadamski.arij.R;
 import com.tadamski.arij.issue.Issue;
 import com.tadamski.arij.login.LoginInfo;
 import com.tadamski.arij.worklog.activity.NewWorklogActivity;
@@ -36,8 +36,8 @@ public class NewWorklogNotificationBuilder {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         Notification notification =
                 new Notification.Builder(ctx).
-                        setLargeIcon(BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_menu_agenda)).
-                        setSmallIcon(R.drawable.ic_menu_agenda).
+                        setLargeIcon(BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_stat_new_worklog)).
+                        setSmallIcon(R.drawable.ic_stat_new_worklog).
                         setOngoing(true).
                         setContentTitle(issue.getSummary().getKey() + ": " + issue.getSummary().getSummary()).
                         setAutoCancel(false).
